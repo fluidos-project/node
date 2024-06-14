d66b57eb2443f116b30e33daa679007e6240a357
Francesco Cappa <24370687+fracappa@users.noreply.github.com>
2024-06-14 15:16:41 +0200

    skip FLUIDOS Node & Liqo installation on a cluster not managed by the user

--- tools/scripts/installation.sh
+++ tools/scripts/installation.sh
@@ -79,7 +79,7 @@ function install_components() {
     local_resource_manager=$4
 
     # Get the kubernetes clusters type from parameters
-    kubernetes_clusters=$5
+    installation_type=$5
 
     helm repo add fluidos https://fluidos-project.github.io/node/
 
@@ -170,26 +170,33 @@ function install_components() {
 
         echo "Providers IPs for cluster $cluster: ${providers_ips[$cluster]}"
 
-        # Get the kubeconfig file which depends on variable kubernetes_clusters
+        # Get the kubeconfig file which depends on variable installation_type
         KUBECONFIG=$(jq -r '.kubeconfig' <<< "${clusters[$cluster]}")
 
 
         echo "The KUBECONFIG is $KUBECONFIG"
 
-        # Check if metrics-server is installed
-        echo "Checking if metrics-server is installed"
-        if ! kubectl get deployment metrics-server -n kube-system --kubeconfig "$KUBECONFIG" &>/dev/null; then
-            echo "Metrics-server is not installed. Installing it..."
-            # Apply the metrics-server
-            kubectl apply -f "$SCRIPT_DIR"/../../quickstart/utils/metrics-server.yaml --kubeconfig "$KUBECONFIG"
-
-            # Wait for the metrics-server to be ready
-            echo "Waiting for metrics-server to be ready"
-            kubectl wait --for=condition=ready pod -l k8s-app=metrics-server -n kube-system --timeout=300s --kubeconfig "$KUBECONFIG"
+        # Skip the installation of the metrics-server if the cluster is a provider and its installation type is not kind
+        if [ "$(jq -r '.role' <<< "${clusters[$cluster]}")" == "provider" ] && [ "$installation_type" != "kind" ]; then
+            echo "Skipping metrics-server installation in a cluster not managed by the user."
         else
-            echo "Metrics-server is already installed"
-        fi
+            # Check if metrics-server is installed
+            echo "Checking if metrics-server is installed"
+            if ! kubectl get deployment metrics-server -n kube-system --kubeconfig "$KUBECONFIG" &>/dev/null; then
+                echo "Metrics-server is not installed. Installing it..."
+                # Apply the metrics-server
+                kubectl apply -f "$SCRIPT_DIR"/../../quickstart/utils/metrics-server.yaml --kubeconfig "$KUBECONFIG"
 
+                # Wait for the metrics-server to be ready
+                echo "Waiting for metrics-server to be ready"
+                kubectl wait --for=condition=ready pod -l k8s-app=metrics-server -n kube-system --timeout=300s --kubeconfig "$KUBECONFIG"
+            else
+                echo "Metrics-server is already installed"
+            fi
+        fi
+        
+
+      
         # Decide value file to use based on the role of the cluster
         if [ "$(jq -r '.role' <<< "${clusters[$cluster]}")" == "consumer" ]; then
             # Check if local resouce manager is enabled
@@ -203,52 +210,67 @@ function install_components() {
             ip=$(jq -r '.ip' <<< "$ip_value")
             port=$consumer_node_port
         else
-            # Check if local resouce manager is enabled
-            if [ "$local_resource_manager" == "true" ]; then
-                value_file="$SCRIPT_DIR/../../quickstart/utils/provider-values.yaml"
+            # Skip this installation if the cluster is a provider and its installation type is not kind
+            if [ "$installation_type" != "kind" ]; then
+                echo "Skipping network configuration in a cluster not managed by the user."
+                return 0
             else
-                value_file="$SCRIPT_DIR/../../quickstart/utils/provider-values-nolrm.yaml"
-            fi
-            # Get cluster IP and port
-            ip_value="${clusters[$cluster]}"
-            ip=$(jq -r '.ip' <<< "$ip_value")
-            port=$provider_node_port
+                # Check if local resouce manager is enabled
+                if [ "$local_resource_manager" == "true" ]; then
+                    value_file="$SCRIPT_DIR/../../quickstart/utils/provider-values.yaml"
+                else
+                    value_file="$SCRIPT_DIR/../../quickstart/utils/provider-values-nolrm.yaml"
+                fi
+                # Get cluster IP and port
+                ip_value="${clusters[$cluster]}"
+                ip=$(jq -r '.ip' <<< "$ip_value")
+                port=$provider_node_port
+                fi
         fi
 
-        # Install the node Helm chart
-        # The installation set statically all the other nodes as providers and the current node as the consumer
-        echo "Installing node Helm chart in cluster $cluster"
-        # If the installation does not use remote repository, the image is used the one built locally
-        if [ "$local_repositories" == "true" ]; then
-            # If the installation does not use remote repository, the CRDs are applied
-            kubectl apply -f "$SCRIPT_DIR"/../../deployments/node/crds --kubeconfig "$KUBECONFIG"
-            echo "Installing local repositories in cluster $cluster with local resource manager"
-            # Execute command
-            # shellcheck disable=SC2086
-            helm upgrade --install node $SCRIPT_DIR/../../deployments/node \
-            -n fluidos --create-namespace -f $value_file $IMAGE_SET_STRING \
-            --set tag=$VERSION \
-            --set "networkManager.configMaps.nodeIdentity.ip=$ip:$port" \
-            --set "networkManager.configMaps.providers.local=${providers_ips[$cluster]}" \
-            --kubeconfig $KUBECONFIG
+
+        # Skipping the installation of the node Helm chart if the cluster is a provider and its installation type is not kind
+        if [ "$(jq -r '.role' <<< "${clusters[$cluster]}")" == "provider" ] && [ "$installation_type" != "kind" ]; then
+            echo "Skipping FLUIDOS Node installation in a cluster not managed by the user"
+            return 0
         else
-            echo "Installing remote repositories in cluster $cluster with local resource manager"
-            helm upgrade --install node fluidos/node -n fluidos --create-namespace -f "$value_file" \
-            --set "networkManager.configMaps.nodeIdentity.ip=$ip:$port" \
-            --set 'networkManager.configMaps.providers.local'="${providers_ips[$cluster]}" \
+            # Install the node Helm chart
+            # The installation set statically all the other nodes as providers and the current node as the consumer
+            echo "Installing node Helm chart in cluster $cluster"
+            # If the installation does not use remote repository, the image is used the one built locally
+            if [ "$local_repositories" == "true" ]; then
+                # If the installation does not use remote repository, the CRDs are applied
+                kubectl apply -f "$SCRIPT_DIR"/../../deployments/node/crds --kubeconfig "$KUBECONFIG"
+                echo "Installing local repositories in cluster $cluster with local resource manager"
+                # Execute command
+                # shellcheck disable=SC2086
+                helm upgrade --install node $SCRIPT_DIR/../../deployments/node \
+                -n fluidos --create-namespace -f $value_file $IMAGE_SET_STRING \
+                --set tag=$VERSION \
+                --set "networkManager.configMaps.nodeIdentity.ip=$ip:$port" \
+                --set "networkManager.configMaps.providers.local=${providers_ips[$cluster]}" \
+                --kubeconfig $KUBECONFIG
+            else
+                echo "Installing remote repositories in cluster $cluster with local resource manager"
+                helm upgrade --install node fluidos/node -n fluidos --create-namespace -f "$value_file" \
+                --set "networkManager.configMaps.nodeIdentity.ip=$ip:$port" \
+                --set 'networkManager.configMaps.providers.local'="${providers_ips[$cluster]}" \
+                --kubeconfig "$KUBECONFIG"
+            fi
+        fi
+
+        # Skip the installation of LIQO if the cluster is a provider and its installation type is not kind
+        if [ "$(jq -r '.role' <<< "${clusters[$cluster]}")" == "provider" ] && [ "$installation_type" != "kind" ]; then
+            echo "Skipping LIQO installation in a cluster not managed by the user."
+        else
+            echo "Installing LIQO in cluster $cluster"
+            echo "Cluster type is $installation_type"
+            liqoctl install "$installation_type" \
+            --set controllerManager.config.resourcePluginAddress=node-rear-controller-grpc.fluidos:2710 \
+            --set controllerManager.config.enableResourceEnforcement=true \
             --kubeconfig "$KUBECONFIG"
         fi
-
-        echo "Installing LIQO in cluster $cluster"
-        echo "Cluster type is $kubernetes_clusters"
-        liqoctl install "$kubernetes_clusters" \
-        --cluster-name "$cluster" \
-        --set controllerManager.config.resourcePluginAddress=node-rear-controller-grpc.fluidos:2710 \
-        --set controllerManager.config.enableResourceEnforcement=true \
-        --kubeconfig "$KUBECONFIG" \
-        --verbose
         ) &
-
         # Save the PID of the process
         pids+=($!)
 
